5029d00e2b24f3bd5180a836dc17568ad4da10ad
MohamedRaslan <mohamedraslang@gmail.com>
2021-06-25 10:23:42 +0200

    Bump version: 0.2.0 → 0.3.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 
 HERE = pathlib.Path(__file__).parent
 
-PACKAGE_VERSION = "0.2.0"
+PACKAGE_VERSION = "0.3.0"
 PACKAGE_NAME = "pytest-qatouch"
 PACKAGE_AUTHOR = "Mohamed Raslan"
 PACKAGE_AUTHOR_EMAIL = "MohamedRaslanG@gmail.com"
